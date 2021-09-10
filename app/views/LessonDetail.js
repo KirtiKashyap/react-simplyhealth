63c6b43837efc7c2e6d61677532cfe75604b7970
KirtiKashyap <kashyap.kirti11@gmail.com>
2021-09-10 12:48:50 +0100

    Update LessonDetail.js

--- app/views/LessonDetail.js
+++ app/views/LessonDetail.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { View,Text } from "react-native";
-import { WebView } from 'react-native-webview';
+import { View, Text } from "react-native";
+import { WebView } from "react-native-webview";
 
-export class LessonDetail extends React.Component{
-
-render(){
-   const tubeId = this.props.route.params.tubeId;
-    console.log(tubeId)
-    let tubeUrl="https://www.youtube.com/embed/"+tubeId;
-return(
-   <View>
-   <Text>{tubeId}</Text>
-<WebView
-style={{margigeTop: 20}}
-javaScriptEnabled={true}
-source={{uri: tubeUrl}}
-/>
-</View>)
-
-}
-   
+export class LessonDetail extends React.Component {
+  render() {
+    const tubeId = this.props.route.params.tubeId;
+    console.log(tubeId);
+    let tubeUrl = "https://www.youtube.com/embed/" + tubeId;
+    return (
+      <View>
+        <Text>{tubeId}</Text>
+        <WebView style={{ margigeTop: 20 }} javaScriptEnabled={true} source={{ uri: tubeUrl }} />
+      </View>
+    );
+  }
 }
